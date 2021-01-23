1b432036f6a00852529b8a2c8485693fe520be22
jarshvader99 <jarshvader@gmail.com>
2021-01-22 23:16:43 -0600

    jquery validation added to contact form

--- JSHDev/wwwroot/js/site.js
+++ JSHDev/wwwroot/js/site.js
@@ -2,3 +2,38 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Wait for the DOM to be ready
+$(function () {
+    // Initialize form validation on the registration form.
+    // It has the name attribute "registration"
+    $("form[name='contactForm']").validate({
+        errorElement: 'div',
+        errorClass: 'errMsg',
+        // Specify validation rules
+        rules: {
+            // The key name on the left side is the name attribute
+            // of an input field. Validation rules are defined
+            // on the right side
+            fromName: "required",
+            emailBody: "required",
+            fromEmail: {
+                required: true,
+                // Specify that email should be validated
+                // by the built-in "email" rule
+                email: true
+            }
+        },
+        errorPlacement: function (error, element) {
+            error.insertAfter(element.parent());  //* THIS IS WHERE I AM HAVING TROUBLE WITH
+        },
+        
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function (form) {
+            $('#send-text').hide();
+            $('#send-loader').show();
+            form.submit();
+        }
+    });
+});
